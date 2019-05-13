b01196dac1f10e83a2c98741c5b5ddd5649fc9c6
Phil Kurth <phil@philkurth.com.au>
2019-05-14 08:35:13 +1000

    Start application object

---
+++ framework/Foundation/Application.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace PdkPluginBoilerplate\Framework\Foundation;
+
+
+use PdkPluginBoilerplate\Framework\Foundation\Providers\WpCliServiceProvider;
+use PdkPluginBoilerplate\Framework\Traits\Singleton;
+
+
+class Application extends Container {
+
+
+	use Singleton;
+
+
+	protected $base_path;
+
+
+	/**
+	 * @var ServiceProviderBase[]
+	 */
+	protected $providers = [];
+
+
+	public function __construct( $base_path = null ) {
+		if ( $base_path ) {
+			$this->set_base_path( $base_path );
+		}
+
+		$this->register_base_bindings();
+		$this->register_base_providers();
+	}
+
+
+	public function set_base_path( $path ) {
+		$this->base_path = rtrim( $path, '\/' );
+	}
+
+
+	public function register_provider( ServiceProviderBase $provider ) {
+		$provider->register( $this );
+		$this->providers[ get_class( $provider ) ] = $provider;
+	}
+
+
+	protected function register_base_bindings() {
+		self::$_instance = $this;
+		$this->singleton( 'app', $this );
+		$this->singleton( Container::class, $this );
+	}
+
+
+	protected function register_base_providers() {
+		$this->register_provider( new WpCliServiceProvider() );
+	}
+
+
+}
