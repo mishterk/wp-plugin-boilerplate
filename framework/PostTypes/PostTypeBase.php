ec16c0e2b23b548b66625910d332d8abae5cd91f
Phil Kurth <phil@philkurth.com.au>
2019-05-24 21:18:18 +1000

    Add doc blocks

--- framework/PostTypes/PostTypeBase.php
+++ framework/PostTypes/PostTypeBase.php
@@ -19,6 +19,13 @@ abstract class PostTypeBase {
 	use DecoratesAndMutatesPostObject;
 
 
+	/**
+	 * Find a post by ID
+	 *
+	 * @param $post_id
+	 *
+	 * @return PostTypeBase|null
+	 */
 	public static function find( $post_id ) {
 		$post = get_post( $post_id );
 
@@ -28,6 +35,13 @@ abstract class PostTypeBase {
 	}
 
 
+	/**
+	 * Decorate an existing post object
+	 *
+	 * @param WP_Post $post
+	 *
+	 * @return PostTypeBase
+	 */
 	public static function make( \WP_Post $post ) {
 		$instance = new static;
 		$instance->set_post_object( $post );
@@ -36,16 +50,27 @@ abstract class PostTypeBase {
 	}
 
 
+	/**
+	 * Register this post type
+	 */
 	public function register() {
 		register_post_type( $this->post_type, $this->get_args() );
 	}
 
 
+	/**
+	 * Get post type args from config
+	 *
+	 * @return array
+	 */
 	public function get_args() {
 		return Application::get_instance()['config']['post-types'][ $this->post_type ] ?? [];
 	}
 
 
+	/**
+	 * Insert/update this post object
+	 */
 	public function save() {
 		wp_insert_post( $this->post->to_array() );
 	}
