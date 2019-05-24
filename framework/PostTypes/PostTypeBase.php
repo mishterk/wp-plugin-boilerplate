fa7b9c19edd4d7f934ea33211ee5901d9e823713
Phil Kurth <phil@philkurth.com.au>
2019-05-24 16:48:10 +1000

    Move documented post props

--- framework/PostTypes/PostTypeBase.php
+++ framework/PostTypes/PostTypeBase.php
@@ -11,30 +11,6 @@ use PdkPluginBoilerplate\Framework\Traits\DecoratesAndMutatesPostObject;
 /**
  * Class PostTypeBase
  * @package PdkPluginBoilerplate\Framework\PostTypes
- *
- * @property int ID
- * @property string post_author
- * @property string post_date
- * @property string post_date_gmt
- * @property string post_content
- * @property string post_title
- * @property string post_excerpt
- * @property string post_status
- * @property string comment_status
- * @property string ping_status
- * @property string post_password
- * @property string post_name
- * @property string to_ping
- * @property string pinged
- * @property string post_modified
- * @property string post_modified_gmt
- * @property string post_content_filtered
- * @property int post_parent
- * @property string guid
- * @property int menu_order
- * @property string post_type
- * @property string post_mime_type
- * @property string comment_count
  */
 abstract class PostTypeBase {
 
